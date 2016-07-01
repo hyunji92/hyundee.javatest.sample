d2e78e5d8166d47739801c1f9bef9cf660b180b0
hyunji <chunghj09@gmail.com>
2016-07-02 00:02:35 +0900

    lambda stream lazy loading 배울꺼야..  stream list

--- ObjectTest/src/LambdaTest.java
+++ ObjectTest/src/LambdaTest.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Created by hyunji on 2016. 6. 29..
@@ -18,7 +19,7 @@ public class LambdaTest {
 
     }
 
-    public static int SumAll(List<Integer > numbers, Predicate<Integer> p){
+    public static int sumAll(List<Integer > numbers, Predicate<Integer> p){
         int total= 0;
         for(int number:numbers){
             if (p.test(number)){
@@ -43,6 +44,14 @@ public class LambdaTest {
         dev.add(new Develop("lambda....",5));
         dev.add(new Develop("c....8....",1));
 
+        Stream<Develop> developStream =
+                dev
+                    .stream()
+                    .peek(d -> d.setCarri(20));
+        System.out.println(dev.get(0).getCarri());
+
+        developStream.count();
+
         printAll(dev);
 
         System.out.println("----------------------------");
@@ -52,5 +61,17 @@ public class LambdaTest {
         System.out.println("----------------------------");
 
         List<Integer> list = Arrays.asList(2,5,7,3,8,10);
+
+        int s =  sumAll(list, n->true);
+        System.out.println(s);
+        list.stream();
+
+
+
+//        LongStream.range(1, 1000000000000000000L);
+
+
+
+
     }
 }
